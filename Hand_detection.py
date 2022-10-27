d51f5603ef98831d763190a9e97e2db4f387f9f8
khushbuchuahan3 <khush441141@gmail.com>
2022-10-27 16:34:26 +0530

    Add files via upload

---
+++ Hand_detection.py
@@ -0,0 +1,15 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+detector=HandDetector(maxHands=2,detectionCon=0.8)
+
+img=cv2.VideoCapture(0)
+while True:
+    cap,frame=img.read()
+    hand,image=detector.findHands(frame)
+    cv2.imshow("frame",frame)
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+img.release()
+cv2.destroyWindow()
